7c213fdeaef38a0468867081ea196510b18c0ba6
kris <r1616306@student.southstaffs.ac.uk>
2017-05-19 10:25:58 +0100

    update step 3

--- scripts/MainGame.js
+++ scripts/MainGame.js
@@ -2,6 +2,8 @@ BasicGame.Game = function (game) {};
 
 //Graphical objects
 var ship;
+var ufos; //group of enemy ufo which drop from the top of the screen
+var lives; //group of lives which are collected
 
 var bullets; //Bullets which your spaceship fires
 var fireRate = 100; //Rate at which bullets are fired
@@ -23,6 +25,26 @@ BasicGame.Game.prototype = {
 			 this.physics.enable(ship, Phaser.Physics.ARCADE);
 			 ship.body.collideWorldBounds = true;
 
+       //Creating groups
+			 //create the ufos group, set the physics and the boundaries
+			 ufos = this.add.group();
+			 this.physics.enable(ufos, Phaser.Physics.ARCADE);
+
+			 ufos.setAll('outOfBoundsKill', true);
+			 ufos.setAll('checkWorldBounds', true);
+			 ufos.setAll('anchor.x', 0.5);
+			 ufos.setAll('anchor.y', 0.5);
+
+			 //create the lives group, set the physics and the boundaries
+			 lives = this.add.group();
+			 this.physics.enable(lives, Phaser.Physics.ARCADE);
+
+			 lives.setAll('outOfBoundsKill', true);
+			 lives.setAll('checkWorldBounds', true);
+			 lives.setAll('anchor.x', 0.5);
+			 lives.setAll('anchor.y', 0.5);
+
+
 			 //Create the bullets group, set the physics, multiples and boundaries
 			 bullets = this.add.group();
 			 bullets.enableBody = true;
@@ -41,6 +63,9 @@ BasicGame.Game.prototype = {
 	update: function () {
 		//execute 'createUfo','createLife','moveShip','collisionDetection' function
       this.moveShip();
+			this.createUfo();
+			this.createLife();
+		},
 			//moves the ship and fires bullet from keyboard controls
 			moveShip: function() {
 				//if left arrow key pressed move players ship left
@@ -62,6 +87,37 @@ BasicGame.Game.prototype = {
 				}
 			},
 
+			//function executed during playing the game to create a ufo
+			createUfo: function () {
+				//Generate a random number between 0 and 20
+				var random = this.rnd.intergerInRange(0, 20);
+				//if random number equals 0 then create a ufo in a random x position and random y velocity
+				if (random === 0) {
+					//Generating random position in the X axis
+					var randomX = this.rnd.intergerInRange(0, this.world.width - 150);
+					//Creating a ufo from the ufos group and seting physics
+					var ufo = ufos.create(randomX, -50, 'ufo');
+					this.physics.enable(ufo, Phaser.Physics.ARCADE);
+					//Generating a random velocity
+					ufo.body.velocity.y = this.rnd.intergerInRange(100, 600);
+				}
+			}
+
+      //function executed during playing the game to create a life
+			createLife: function () {
+				//generate random number between 0 and 500
+				var random = this.rnd.intergerInRange(0, 500);
+				//if random number equals 0 then create a life in a random x position
+				if (random === 0) {
+					//generating random position in the X axis
+					var randomX = this.rnd.intergerInRange(0, this.world.width - 150);
+					//creating a ufo from the ufos group and setting physics
+					var life = lives.create(randomX, -50, 'life');
+					this.physics.enable(life, Phaser.Physics.ARCADE);
+					//Generating a random velocity
+					life.body.velocity.y = 150;
+				}
+			}
 			//Generate bullet and position in the x axis, set the velocity and play the audio
 			fireBullet: function () {
 				if (this.time.now > nextFire && bullets.countDead() > 0) {
--- scripts/Preloader.js
+++ scripts/Preloader.js
@@ -21,6 +21,8 @@ BasicGame.Preloader.prototype = {
 			this.load.image('starfield', 'assets/starfield.png');
 			this.load.image('startButton', 'assets/startButton.png');
 			this.load.image('ship', 'assets/ship.png');
+			this.load.image('ufo', 'assets/ufo.png');
+			this.load.image('life', 'assets/lives.png');
 			this.load.image('bullet', 'assets/bullet.png');
 		},
 
